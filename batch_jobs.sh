cf96488b06f562bbe1eb8a8f6e489692459e9156
Skovorp <peter.skovorodnikov@gmail.com>
2025-10-24 17:42:22 -0400

    configs

--- batch_jobs.sh
+++ batch_jobs.sh
@@ -5,7 +5,7 @@ CODE_DIR="/home/petr/video_understanding"
 PYTHON_ENV="/home/petr/miniconda3/envs/feral/bin/python"
 
 for config in "$CONFIG_DIR"/*.yaml; do
-    srun -p h100 --gres=gpu:1 -w gpu001 --cpus-per-task=12 \
+    srun -p l40s --gres=gpu:1 -w gpu101 --cpus-per-task=12 \
         /bin/bash -c "cd $CODE_DIR && $PYTHON_ENV train.py $config" &
 done
 

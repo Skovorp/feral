c65cf69302217313173b84dd45977dd44b88b839
Peter Skovorodnikov <46576943+Skovorp@users.noreply.github.com>
2025-11-20 02:56:05 +0300

    add max_num_worker util

--- utils.py
+++ utils.py
@@ -90,5 +90,20 @@ def next_nonzero_index(arr):
         out[i] = next_idx
     return out
 
+def suggested_num_workers():
+    max_num_worker_suggest = None
+    if hasattr(os, 'sched_getaffinity'):
+        try:
+            max_num_worker_suggest = len(os.sched_getaffinity(0))
+        except Exception:
+            pass
+    if max_num_worker_suggest is None:
+        # os.cpu_count() could return Optional[int]
+        # get cpu count first and check None in order to satify mypy check
+        cpu_count = os.cpu_count()
+        if cpu_count is not None:
+            max_num_worker_suggest = cpu_count
+    return max_num_worker_suggest
+
 if __name__ == "__main__":
     print(next_nonzero_index([1., 0., 0., 1., 0., 0., 1., 0., 0., 1., 0]))
